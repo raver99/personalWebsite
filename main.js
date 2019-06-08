49700aa0235914090c26024483273293e4983130
Roland <macintoshhd@Macintoshs-MacBook-Pro.local>
2019-06-08 13:11:25 +0300

    implemented second slide

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('test');
+var mySidenav = $('#mySidenav');
+
+function openNav() {
+  mySidenav.height("100%");
+  $('body').addClass("fixedPosition");
+}
+  
+function closeNav() {
+    mySidenav.height("0%");
+    $('body').removeClass("fixedPosition");
+}
+
+$(document).ready(function () {
+    var menuButton = $('#menuButton');
+    
+    menuButton.click(function() {
+        menuButton.toggleClass("menu-button-animate");
+        openNav();
+      });
+   });
+
+  
