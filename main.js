368fe20c45ca04dc8ec2c30fcab0507e24e9e6d6
Roland <macintoshhd@Macintoshs-MacBook-Pro.local>
2019-06-11 17:26:40 +0300

    updated animation code

--- main.js
+++ main.js
@@ -11,12 +11,16 @@ function closeNav() {
 }
 
 $(document).ready(function () {
+
+    //menu button animation
     var menuButton = $('#menuButton');
     
     menuButton.click(function() {
         menuButton.toggleClass("menu-button-animate");
         openNav();
       });
+
+      var isPhone = window.matchMedia("(max-width: 480px)").matches;
 
       var controller = new ScrollMagic.Controller();
 
@@ -27,61 +31,48 @@ $(document).ready(function () {
       .addIndicators({name: "pin"}) // add indicators (requires plugin)
       .addTo(controller);
 
-      //scale animation
-      var fadeInTimeline = new TimelineMax();
-      var fadeInFrom = TweenMax.from("#iphone", 1, {
-          scale: 1.5,  transformOrigin:'50% 0%'
-      });
-      var fadeInTo = TweenMax.to("#iphone", 1, {
-          scale: 1, transformOrigin:'50% 0%'
-      });
-      var tweenTranslate = TweenMax.to('#iphone-container', 1, { css: { transform: 'translate3d(-30%, 0 , 0)' }});
+    var animationtimeLine = new TimelineMax();
 
-      var tweenFade = TweenMax.to('#iphone-apps', 1, { css: { opacity: 1}});
-      var tweenFade2 = TweenMax.to('#projects-text', 1, { css: { opacity: 1}});
+    //make smaller animation
+    var tweenSmaller;
+    if(isPhone){
+        tweenSmaller = TweenMax.to([".iphone", "iphone-apps"], 1, {
+            width: "60%"
+        }, "iphone-smaller");
+    }else{
+        tweenSmaller = TweenMax.to([".iphone", "iphone-apps"], 1, {
+            height: "500px"
+        }, "iphone-smaller");
+    }
 
-      fadeInTimeline
-          .add(fadeInFrom)
-          .add(fadeInTo)
-          .add(tweenTranslate)
-          .add(tweenFade)
-          .add(tweenFade2);
+    //fade in apps animation
+    var tweenFade = TweenMax.to('#iphone-apps', 1, { css: { opacity: 1}});
       
-      new ScrollMagic.Scene({
-              triggerElement: "#sticky-container"
-          })
-      .setTween(fadeInTimeline)
-      .offset(400)
-      .duration("300%")
-      .addIndicators({name: "fade in"}) // add indicators (requires plugin)
-      .addTo(controller);
+    //slide left animation
+    var tweenTranslate;
+    if(!isPhone){
+        tweenTranslate = TweenMax.to('#iphone-container', 1, { css: { transform: 'translate3d(-30%, 0 , 0)' }});
+    }
+    
+    //reveal text
+    var tweenFadeProjectText = TweenMax.to('#projects-text', 1, { css: { opacity: 1}});
 
-    //left to right
-    // var fromLeftTimeline = new TimelineMax();
-    // var fromLeftFrom = TweenMax.from("#iphone", 1, {
-    //     x: 500
-    // });
-    // var fromLeftTo = TweenMax.to("#iphone", 1, {
-    //     x: 0
-    // });
-    // fromLeftTimeline
-    //     .add(fromLeftFrom)
-    //     .add(fromLeftTo);
-
-
-    //var tweenTranslate = new TweenMax.to('#iphone-container', 1, { css: { transform: 'translate3d(-150px, 0 , 0)' }})
-
-    // // slide scene
-    // var scene2 = new ScrollMagic.Scene({
-    //       triggerElement: "#iphone-container"
-    //   })
-    // scene2.offset(1000);
-    // // scene2.triggerHook(0.8);
-    // scene2.setTween(tweenTranslate)
-    // .duration(500)
-    // //    .reverse(false)
-    // .addIndicators({name: "slide left"}) // add indicators (requires plugin)
-    // .addTo(controller);
+    //add animations to timeline
+    animationtimeLine.add(tweenSmaller);
+    animationtimeLine.add(tweenFade);
+    if(tweenTranslate){
+        animationtimeLine.add(tweenTranslate);
+    }
+    animationtimeLine.add(tweenFadeProjectText);
+    
+    new ScrollMagic.Scene({
+            triggerElement: "#sticky-container"
+        })
+    .setTween(animationtimeLine)
+    .offset(400)
+    .duration("300%")
+    .addIndicators({name: "animations"}) // add indicators (requires plugin)
+    .addTo(controller);
 });
 
   
